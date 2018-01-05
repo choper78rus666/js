5865aec51ca3e73bff5aef4a620187c2778a9f5a
choper78rus666 <driver-tt@mail.ru>
2018-01-06 00:45:47 +0300

    Бонусы теперь падают только на белое поле, поправил движение мыши

--- maze_Gleb&Dmitriy/js/mazeController.js
+++ maze_Gleb&Dmitriy/js/mazeController.js
@@ -14,7 +14,7 @@ class CanvasDrawer {
     generateImage(img_path, coord, what_draw) {
         let img = document.createElement('img');
         img.setAttribute("src", img_path);
-        if(what_draw === 1){ //1 ресуем поле 2- рисуем мышку 3 - рисуем бонус
+        if(what_draw === 1){ //1 рисуем поле 2- рисуем мышку 3 - рисуем бонус
             this.canvas.height = img.height;
             this.canvas.width = img.width;
             this.canvas_context.drawImage(img, coord[0], coord[1]);
@@ -30,7 +30,7 @@ class CanvasDrawer {
     clear(set){
         // this.canvas_context.fillStyle = '255 255 255 255';
         this.canvas_context.fillStyle = '#FFF';
-        this.canvas_context.fillRect(set[0]+1,set[1],23,24);
+        this.canvas_context.fillRect(set[0],set[1],26,26);
         // this.canvas_context.fillRect(set[0],set[1],23,24);
         // let ctx = this.canvas_context;
         // ctx.beginPath();
@@ -68,27 +68,41 @@ class HumanController extends CanvasDrawer{
         window.addEventListener('keypress', this.someFunc.bind(this))
     }
 
-    calcCollision(coord,what_need_calc){ //
+    calcCollision(coord, what_need_calc,){ //
         if(what_need_calc === 'human'){
-        let midUp = this.canvas_context.getImageData(coord[0]+12,coord[1]-2,1,1).data;
-        let midRight = this.canvas_context.getImageData(coord[0]+28,coord[1]+12,1,1).data;
-        let midDown = this.canvas_context.getImageData(coord[0]+13,coord[1]+28,1,1).data;
-        let midLeft = this.canvas_context.getImageData(coord[0]-2,coord[1]+14,1,1).data;
-        let stop = [midUp[0],midRight[0],midDown[0],midLeft[0]];
-        console.log(stop);
-        fieldController.field_model.stop = [midUp,midRight,midDown,midLeft];
-        // let black = humanData.prototype.entries([0],255);
-        console.log("Левый угол верх",midUp[0],'Альфа',midUp[3],'Правый верх угол',midRight[0],'Альфа',midRight[3],"Правый угол низ",midDown[0],'Альфа',midDown[3],'Левый угол Низ',midLeft[0],'Альфа', midLeft[3]);
-        } else if(what_need_calc === 'bonus'){
-            let sqare = this.canvas_context.getImageData(coord[0],coord[1],26,26);
+            let midUp = this.canvas_context.getImageData(coord[0],coord[1]-3,25,1).data;
+            let midRight = this.canvas_context.getImageData(coord[0]+27,coord[1],1,25).data;
+            let midDown = this.canvas_context.getImageData(coord[0],coord[1]+27,25,1).data;
+            let midLeft = this.canvas_context.getImageData(coord[0]-3,coord[1],1,25).data;
+            fieldController.field_model.stop = [checkCoord(midUp),checkCoord(midRight),checkCoord(midDown),checkCoord(midLeft)];
+           // stop = [checkCoord(midUp),checkCoord(midRight),checkCoord(midDown),checkCoord(midLeft)];
+            //console.log(stop);
             
+            // let black = humanData.prototype.entries([0],255);
+            //console.log("Левый угол верх",midUp[0],'Альфа',midUp[3],'Правый верх угол',midRight[0],'Альфа',midRight[3],"Правый угол низ",midDown[0],'Альфа',midDown[3],'Левый угол Низ',midLeft[0],'Альфа', midLeft[3]);
+        } else if(what_need_calc === 'bonus'){
+            let sqare = this.canvas_context.getImageData(coord[0]-1,coord[1]-1,27,27).data;
+                if(checkCoord(sqare) !== 255){
+                    fieldController.field_model.bonusCollision = true;
+                    //console.log("координата не удачна",checkCoord(sqare, 255));
+                }
+        }
+        
+        function checkCoord(mass){
+            for(var i = 0; i < mass.length; i++){
+                if(mass[i] !== 255){
+                    return mass[i];
+                    break;
+                }
+            }
+            return 255;
         }
     }
     
     someFunc(event) {
         // event.preventDefault();
-        console.log(event.keyCode);
-        console.log(typeof event);
+        //console.log(event.keyCode);
+       // console.log(typeof event);
         this.sendToModel(event);
     }
 
--- maze_Gleb&Dmitriy/js/mazeModel.js
+++ maze_Gleb&Dmitriy/js/mazeModel.js
@@ -5,7 +5,7 @@ class FieldModel {
         this.finish_coord = [];
         this.coord = [444, 11];
         this.bonusCoord = [];
-        this.bonuCollision = false;
+        this.bonusCollision = false;
         this.stop = [0, 0, 0, 0];
         this.event = 0;
         console.log(this);
@@ -18,25 +18,25 @@ class FieldModel {
                         //отрисовываем
         let set = this.coord;
         fieldController.clear(set);
-        console.log('Координаты для для обработки',this.coord);
+       // console.log('Координаты для для обработки',this.coord);
         human.calcCollision(this.coord,'human');
-        console.log('Параметры для коллизии',this.stop);
+       // console.log('Параметры для коллизии',this.stop);
         // console.log(this.stop[0], this.stop[1], this.stop[2] !== 255,this.stop[3][3]);
         let u = this.stop[0];
         let r = this.stop[1];
         let d = this.stop[2];
         let l = this.stop[3];
-            console.log(u[0] !== 255);
-            console.log(u,r,d,l);
+            //console.log(u[0] !== 255);
+           // console.log(u,r,d,l);
 
-        if(event.keyCode === 38 && u[0] === 255){
-            this.coord[1] -=5
-        } else if(event.keyCode === 39 && r[0] === 255) {
-            this.coord[0] +=5
-        } else if(event.keyCode === 40 && d[0] === 255){
-            this.coord[1] +=5
-        }else if(event.keyCode === 37 && l[0] === 255){
-            this.coord[0] -=5
+        if(event.keyCode === 38 && u === 255){
+            this.coord[1] -=3
+        } else if(event.keyCode === 39 && r === 255) {
+            this.coord[0] +=3
+        } else if(event.keyCode === 40 && d === 255){
+            this.coord[1] +=3
+        }else if(event.keyCode === 37 && l === 255){
+            this.coord[0] -=3
         } else {console.log('error')}
         human.generateImage('img/mouse.png', this.coord, 2);
         //if(!this.bonusCoord[0])
@@ -53,12 +53,14 @@ class FieldModel {
         setInterval(function(){
             if(i < 10){
                     let coord = [parseInt(Math.random()*966),parseInt(Math.random()*966)];
-                    human.calcCollision(coord);
-                    if(self.bonuCollision === false){
-                        console.log('Наши координаты бонуса',coord);
+                    human.calcCollision(coord, 'bonus');
+                    if(self.bonusCollision === false){
+                        //console.log('Наши координаты бонуса',coord);
                         human.generateImage('img/bonus.png',coord,3); //TODO: создать class bonus
                         self.bonusCoord.push(coord); // TODO: раскидать на методы 1 метод одно действие
                         i++;
+                    } else {
+                        self.bonusCollision = false;
                     }
             } else {
                 self.bonusClear();
